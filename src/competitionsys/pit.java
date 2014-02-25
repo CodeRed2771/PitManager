e9bb69589915a6933199ce98a72a2e0f89c41191
Michael Spoehr <spoehrmichael@gmail.com>
2014-02-25 15:48:53 -0500

    Created new project

---
+++ src/competitionsys/pit.java
@@ -0,0 +1,16 @@
+package competitionsys;
+
+/**
+ *
+ * @author Michael
+ */
+public class pit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
