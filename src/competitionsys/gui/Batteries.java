19de207f7913ec4123771199acac080537010048
WSPStudent <WSPStudent@WSP519.att.net>
2014-03-27 20:13:19 -0400

    Battery tab

--- src/competitionsys/gui/Batteries.java
+++ src/competitionsys/gui/Batteries.java
@@ -2,6 +2,9 @@ package competitionsys.gui;
 
 import competitionsys.persistence.Battery;
 import competitionsys.persistence.Competition;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.util.ArrayList;
 
 /**
@@ -16,34 +19,56 @@ public class Batteries extends javax.swing.JPanel {
      * Creates new form Batteries
      */
     public Batteries() {
-        initComponents();
+//        initComponents();
+//
+//        batteries = Competition.getInstance().getBatteries();
+//        ArrayList<String> model = new ArrayList<>();
+//        for (Battery batt : batteries) {
+//            model.add(batt.getBatteryLabel());
+//        }
+//        String[] array = new String[model.size()];
+//        for (int i = 0; i < model.size(); i++) {
+//            array[i] = model.get(i);
+//        }
+//        battery.setModel(new javax.swing.DefaultComboBoxModel<>(array));
+        repaint();
+    }
 
-        batteries = Competition.getInstance().getBatteries();
-        ArrayList<String> model = new ArrayList<>();
-        for (Battery batt : batteries) {
-            model.add(batt.getBatteryLabel());
+//    public void init() {
+//        loadNotes();
+//    }
+//
+//    private void saveNotes() {
+//        batteries.get(battery.getSelectedIndex()).setBatteryNotes(notes.getText());
+//        batteries.get(battery.getSelectedIndex()).setBatteryStatus(status.getText());
+//        batteries.get(battery.getSelectedIndex()).setBatteryCharge(charge.getText());
+//    }
+//
+//    private void loadNotes() {
+//        notes.setText(batteries.get(battery.getSelectedIndex()).getBatteryNotes());
+//        status.setText(batteries.get(battery.getSelectedIndex()).getBatteryStatus());
+//        charge.setText(batteries.get(battery.getSelectedIndex()).getBatteryCharge());
+//    }
+    @Override
+    public void paintComponent(Graphics g) {
+        Image dbImg = createImage(getWidth(), getHeight());
+        Graphics dbg = dbImg.getGraphics();
+        draw(dbg);
+        g.drawImage(dbImg, 0, 0, this);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        for (int i = 10; i <= 130; i += 60) {
+            g.drawRect(i, 10, 60, 30);
         }
-        String[] array = new String[model.size()];
-        for (int i = 0; i < model.size(); i++) {
-            array[i] = model.get(i);
+        for (int x = 10; x <= 130; x += 60) {
+            for (int y = 40; y <= 280; y += 60) {
+                g.drawRect(x, y, 60, 60);
+            }
         }
-        battery.setModel(new javax.swing.DefaultComboBoxModel<>(array));
-    }
-
-    public void init() {
-        loadNotes();
-    }
-
-    private void saveNotes() {
-        batteries.get(battery.getSelectedIndex()).setBatteryNotes(notes.getText());
-        batteries.get(battery.getSelectedIndex()).setBatteryStatus(status.getText());
-        batteries.get(battery.getSelectedIndex()).setBatteryCharge(charge.getText());
-    }
-
-    private void loadNotes() {
-        notes.setText(batteries.get(battery.getSelectedIndex()).getBatteryNotes());
-        status.setText(batteries.get(battery.getSelectedIndex()).getBatteryStatus());
-        charge.setText(batteries.get(battery.getSelectedIndex()).getBatteryCharge());
+       g.setColor(Color.blue);
+       g.fillRect(250, 10, 80, 100);
     }
 
     /**
@@ -56,154 +81,41 @@ public class Batteries extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        battery = new javax.swing.JComboBox<String>();
-        notes = new javax.swing.JTextField();
-        status = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        charge = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(0, 0, 0));
-
-        battery.setBackground(new java.awt.Color(0, 0, 0));
-        battery.setForeground(new java.awt.Color(255, 255, 255));
-        battery.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        battery.setFocusable(false);
-        battery.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                batteryActionPerformed(evt);
-            }
-        });
-
-        notes.setBackground(new java.awt.Color(0, 0, 0));
-        notes.setForeground(new java.awt.Color(255, 255, 255));
-        notes.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                notesActionPerformed(evt);
-            }
-        });
-
-        status.setBackground(new java.awt.Color(0, 0, 0));
-        status.setForeground(new java.awt.Color(255, 255, 255));
-        status.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                statusActionPerformed(evt);
-            }
-        });
-
-        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("BATTERIES");
-
-        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel4.setText("Status:");
-
-        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel5.setText("Charge:");
-
-        charge.setBackground(new java.awt.Color(0, 0, 0));
-        charge.setForeground(new java.awt.Color(255, 255, 255));
-        charge.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                chargeActionPerformed(evt);
-            }
-        });
-
-        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/competitionsys/gui/battery.png"))); // NOI18N
-
-        jLabel7.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel7.setText("Notes:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(612, Short.MAX_VALUE)
+                .addContainerGap(720, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(88, 88, 88)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel4)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel7))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(charge)
-                            .addComponent(status)
-                            .addComponent(notes, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(battery, javax.swing.GroupLayout.Alignment.TRAILING, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(118, 118, 118))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(battery, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(notes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel7))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(status, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel4))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(charge, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel5)))
-                    .addComponent(jLabel6))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addContainerGap(477, Short.MAX_VALUE)
                 .addComponent(jLabel1))
         );
     }// </editor-fold>//GEN-END:initComponents
-
-    private void batteryActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_batteryActionPerformed
-        loadNotes();
-    }//GEN-LAST:event_batteryActionPerformed
-
-    private void notesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_notesActionPerformed
-        saveNotes();
-    }//GEN-LAST:event_notesActionPerformed
-
-    private void statusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_statusActionPerformed
-        saveNotes();
-    }//GEN-LAST:event_statusActionPerformed
-
-    private void chargeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chargeActionPerformed
-        saveNotes();
-    }//GEN-LAST:event_chargeActionPerformed
-
-
+//    public void init() {
+//        loadNotes();
+//    }
+//
+//    private void saveNotes() {
+//        batteries.get(battery.getSelectedIndex()).setBatteryNotes(notes.getText());
+//        batteries.get(battery.getSelectedIndex()).setBatteryStatus(status.getText());
+//        batteries.get(battery.getSelectedIndex()).setBatteryCharge(charge.getText());
+//    }
+//
+//    private void loadNotes() {
+//        notes.setText(batteries.get(battery.getSelectedIndex()).getBatteryNotes());
+//        status.setText(batteries.get(battery.getSelectedIndex()).getBatteryStatus());
+//        charge.setText(batteries.get(battery.getSelectedIndex()).getBatteryCharge());
+//    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<String> battery;
-    private javax.swing.JTextField charge;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JTextField notes;
-    private javax.swing.JTextField status;
     // End of variables declaration//GEN-END:variables
-
 }
